d687f19f4e636139ca1209430e72b5f4fe524f4e
espen357moe <espen357moe@gmail.com>
2016-01-21 11:58:22 +0100

    Initial commit

---
+++ KarakterKalkis/Fag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterKalkis
+{
+    public enum LovligKarakter { A = 5, B = 4, C = 3, D = 2, E = 1, F = 0 };
+
+    class Fag //Et fag består av et vekttall som er enten 7.5 eller 15, og en bokstavkarakter som korresponderer med en heltallsverdi
+    {        
+        private LovligKarakter _karakter;
+        private float _vekttall;
+
+        public char BokstavKarakter
+        {
+            get { return _karakter.ToString().First(); }
+            set { _karakter = (LovligKarakter)Enum.Parse(typeof(LovligKarakter), value.ToString()); }        
+        }
+
+        public float Vekttall
+        {
+            get { return this._vekttall; }
+            set { this._vekttall = value; }
+        }
+    }
+}
---
+++ KarakterKalkis/FagViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KarakterKalkis
+{
+    class FagViewModel
+    {
+        public ObservableCollection<Fag> FagListe = new ObservableCollection<Fag>();
+        
+        public void addFag()
+        {
+            FagListe.Add(new Fag());
+        }
+        
+        public void removeFag()
+        {
+            var indexOfLastElement = (FagListe.Count)-1;
+            FagListe.RemoveAt(indexOfLastElement);           
+        }
+    }
+}
